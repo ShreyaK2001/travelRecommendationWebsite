13f2b19540b1f0de18531595628a067556eee18a
ShreyaK2001 <shreyakaushal14@gmail.com>
2024-12-20 11:47:09 -0500

    Updated style.css and js file

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,52 +1,65 @@
 
 const searchBtn = document.getElementById("search");
 const clearBtn = document.getElementById("clear");
-const keyword = document.getElementById("searchBar").value.toLowerCase();
 const overlay = document.getElementById("rec");
 let recommendations = [];
 let results =[];
 
-searchBtn.addEventListener("click", displayRec);
+searchBtn.addEventListener("click", fetchResults);
 clearBtn.addEventListener("click", clearResults);
 
-fetch("travel_recommendation_api.json")
-.then(response => response.json())
-.then(data =>{
-    recommendations = data;
-})
-.catch(error => console.error("Error fetching json data", error));
+function fetchResults(){
+    const keyword = document.getElementById("searchBar").value.toLowerCase();
+    if(!keyword){
+        alert("Please enter a keyword to search.");
+        return;
+    }
 
+    fetch("travel_recommendation_api.json")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Network was not okay!");
+        }
+        return response.json();
+    })
+    .then(data =>{
+        let results =[];
+        recommendations = data;
 
-function displayRec(keyword){
-    
-    const country = recommendations.countries.find(country => country.name.toLowerCase() === keyword);
+        const country = recommendations.countries.find(country => country.name.toLowerCase() === keyword);
     if(country){
         results = country.cities.map(city =>({
             name : city.name,
-            imageURL : city.imageURL,
+            imageURL : city.imageUrl,
             description : city.description
         }));
     }
     if(keyword === "temple"){
         results = recommendations.temples.map(tem =>({
             name: tem.name,
-            imageURL : tem.imageURL,
+            imageURL : tem.imageUrl,
             description : tem.description
         }));
     }
     if(keyword === "beach"){
         results = recommendations.beaches.map(bea => ({
             name : bea.name,
-            imageURL : bea.imageURL,
+            imageURL : bea.imageUrl,
             description : bea.description
         }));
     }
 
     displayResults(results);
+    })
+    .catch(error => {
+        console.error("Error fetching JSON data: ", error);
+        alert("Unable to fetch data. Please try again later");
+    });
 }
 
+
 function displayResults(results){
-    const resultsDiv = document.getElementById('rec');
+    const resultsDiv = document.getElementById("rec");
     resultsDiv.innerHTML = "";
 
     if(results.length === 0){
@@ -65,8 +78,9 @@ function displayResults(results){
     }
     resultsDiv.style.display = "block";
 }
+
 function clearResults(){
-    document.getElementById('searchBar').value ="";
-    document.getElementById('rec').innerHTML ="";
+    document.getElementById('searchBar').value = "";
+    document.getElementById('rec').innerHTML = "";
     document.getElementById('rec').style.display= "none";
 }
